d341d3156e3b9f5b9a527669aaba9aaf58952501
AhmedNasr <63086808+ahmednasr8898@users.noreply.github.com>
2022-05-12 09:58:24 +0200

    Initial Commit

---
+++ RecognizeHandwriting-with-CoreML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecognizeHandwriting-with-CoreML
+//
+//  Created by Nasr on 12/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
