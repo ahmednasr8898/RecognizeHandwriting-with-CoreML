44bb60f734ffc459da115ec05f2eacbb353ff5f7
AhmedNasr <63086808+ahmednasr8898@users.noreply.github.com>
2022-05-12 10:42:16 +0200

    All Project

--- RecognizeHandwriting-with-CoreML/ViewController.swift
+++ RecognizeHandwriting-with-CoreML/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var borderView: CanvasView!
+    
+    var requests = [VNRequest]() // holds Image Classification Request
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupVision()
     }
-
-
+    
+    func setupVision() {
+        // load MNIST model for the use with the Vision framework
+        guard let visionModel = try? VNCoreMLModel(for: MNIST().model) else {fatalError("can not load Vision ML model")}
+        
+        // create a classification request and tell it to call handleClassification once its done
+        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: self.handleClassification)
+        
+        self.requests = [classificationRequest] // assigns the classificationRequest to the global requests array
+        
+    }
+    
+    func handleClassification (request:VNRequest, error:Error?) {
+        guard let observations = request.results else {print("no results"); return}
+        
+        // process the ovservations
+        let classifications = observations
+            .flatMap({$0 as? VNClassificationObservation}) // cast all elements to VNClassificationObservation objects
+            .filter({$0.confidence > 0.8}) // only choose observations with a confidence of more than 80%
+            .map({$0.identifier}) // only choose the identifier string to be placed into the classifications array
+        
+        DispatchQueue.main.async {
+            self.resultLabel.text = classifications.first // update the UI with the classification
+        }
+        
+    }
+    
+    
+    @IBAction func didPressedOnClearButton(_ sender: Any) {
+        borderView.clearCanvas()
+    }
+    
+    @IBAction func didPressedOnReadButton(_ sender: Any) {
+        
+        let image = UIImage(view: borderView) // get UIImage from CanvasView
+        let scaledImage = scaleImage(image: image, toSize: CGSize(width: 28, height: 28)) // scale the image to the required size of 28x28 for better recognition results
+        
+        let imageRequestHandler = VNImageRequestHandler(cgImage: scaledImage.cgImage!, options: [:]) // create a handler that should perform the vision request
+        
+        do {
+            try imageRequestHandler.perform(self.requests)
+        }catch{
+            print("ERROR")
+            print(error)
+        }
+    }
+    
+    // scales any UIImage to a desired target size
+    func scaleImage (image:UIImage, toSize size:CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+    
 }
 
